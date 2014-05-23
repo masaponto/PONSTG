1625c53be51e9f4d5938302f0be6397d4373f9ed
masaponto <masaponto@gmail.com>
2014-05-24 00:54:58 +0900

    toolbar 消すとエラーになる。よくわからない

--- app/src/main/java/com/masaponto/android/ponstg/TitleActivity.java
+++ app/src/main/java/com/masaponto/android/ponstg/TitleActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageButton;
 
 
