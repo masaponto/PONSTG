b5e0756df1cbe9ac272aa5f6cdb63a50e156203b
masaponto <masaponto.web@gmail.com>
2014-05-24 23:14:54 +0900

    自機、敵の描画方法を変更

--- app/src/main/java/com/masaponto/android/ponstg/Chara.java
+++ app/src/main/java/com/masaponto/android/ponstg/Chara.java
@@ -4,12 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.Rect;
 
 class Chara{
 
     private Bitmap charaImage;
-    private Rect charaSrc, charaDst;
     private Point p;
     private Paint paint;
 
@@ -17,61 +15,52 @@ class Chara{
 
     private int displayX, displayY;
 
-    private int scale;
-    private int sizeX, sizeY;
-
-    Chara(Bitmap charaImage, int displayX, int displayY, int sizeX, int sizeY, int scale){
+    Chara(Bitmap charaImage, int displayX, int displayY, int scale){
         this.charaImage = charaImage;
         this.displayX = displayX;
         this.displayY = displayY;
-        this.scale = scale;
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
 
         p = new Point();
         paint = new Paint();
-        charaSrc = new Rect(0, 0, charaImage.getWidth(), charaImage.getHeight());
 
         init(displayX, displayY);
     }
 
     public void init(int displayX, int displayY){
-        p.x = displayX / 2 - (sizeX) / 2;
-        p.y = displayY * 2 / 3 - (sizeY) * 3;
-        charaDst = new Rect(p.x, p.y, p.x + sizeX, p.y + sizeY);
+        p.x = displayX / 2 - (charaImage.getWidth()) / 2;
+        p.y = displayY * 2 / 3 - (charaImage.getHeight()) * 3;
     }
 
     public void move(int x, int y){
-        p.x = x - (sizeX) / 2;
-        p.y = y - (sizeY) / 2;
+        p.x = x - (charaImage.getWidth()) / 2;
+        p.y = y - (charaImage.getHeight()) / 2;
 
         //画面外のでないように処理
         if(p.x < 0){
             p.x = 0;
         }
-        if(p.x + (sizeX) > displayX){
-            p.x = displayX - (sizeX);
+        if(p.x + (charaImage.getWidth()) > displayX){
+            p.x = displayX - (charaImage.getWidth());
         }
         if(p.y < 0){
             p.y = 0;
         }
-        if(p.y + (sizeY) > displayY * 3/4){
-            p.y = displayY * 3/4 - sizeY;
+        if(p.y + (charaImage.getHeight()) > displayY * 3/4){
+            p.y = displayY * 3/4 - charaImage.getHeight();
         }
 
-        charaDst = new Rect(p.x, p.y, p.x + sizeX, p.y + sizeY);
     }
 
     public int getCenterX(){
-        return p.x + (sizeX) / 2;
+        return p.x + (charaImage.getWidth()) / 2;
     }
 
     public int getCenterY(){
-        return p.y + (sizeY) / 2;
+        return p.y + (charaImage.getHeight()) / 2;
     }
 
     public void drawMove(Canvas c){
-        c.drawBitmap(charaImage, charaSrc, charaDst, paint);
+        c.drawBitmap(charaImage, p.x, p.y, paint);
     }
 
 }
--- app/src/main/java/com/masaponto/android/ponstg/Enemy.java
+++ app/src/main/java/com/masaponto/android/ponstg/Enemy.java
@@ -2,39 +2,30 @@ package com.masaponto.android.ponstg;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.Rect;
 
 
 class Enemy{
 
-    private Bitmap enemyImage;
-    private Rect enemySrc, enemyDst;
+    private Bitmap[] enemyImage;
     private Point p;
     private Paint paint;
 
     private int enemyCenterX;
     private int enemyCenterY;
     private int displayX, displayY;
-    private int sizeX, sizeY;
+
     private int scale;
     private int moveSpeed;
 
-    double hitDistance;
+    public double hitDistance;
 
     public boolean deathFlag = false;
 
-    //add
-    Matrix matrix1 = new Matrix();
-    Matrix matrix2 = new Matrix();
-    Matrix matrix3 = new Matrix();
-    Matrix matrix4 = new Matrix();
-    Bitmap enemy1, enemy2, enemy3, enemy4;
     private int count;
 
-    Enemy(Bitmap enemyImage, int w, int h, int displayX, int displayY, int sizeX, int sizeY, int scale, int moveSpeed){
+    Enemy(Bitmap[] enemyImage, int w, int h, int displayX, int displayY, int scale, int moveSpeed){
         this.enemyImage = enemyImage;
         p = new Point();
         paint = new Paint();
@@ -43,32 +34,19 @@ class Enemy{
         this.scale = scale;
         this.displayX = displayX;
         this.displayY = displayY;
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
+
         this.moveSpeed = moveSpeed;
 
-        enemyCenterX = p.x + (sizeX) / 2;
-        enemyCenterY = p.y + (sizeY) / 2;
-
-        matrix1.postScale(0.1F * scale, 0.1F * scale);
-        matrix2.postRotate(22.5F);
-        matrix2.postScale(0.1F * scale, 0.1F * scale);
-        matrix3.postRotate(45F);
-        matrix3.postScale(0.1F * scale, 0.1F * scale);
-        matrix4.postRotate(67.5F);
-        matrix4.postScale(0.1F * scale, 0.1F * scale);
-        enemy1 = Bitmap.createBitmap(enemyImage, 0, 0, enemyImage.getWidth(), enemyImage.getHeight(), matrix1, true);
-        enemy2 = Bitmap.createBitmap(enemyImage, 0, 0, enemyImage.getWidth(), enemyImage.getHeight(), matrix2, true);
-        enemy3 = Bitmap.createBitmap(enemyImage, 0, 0, enemyImage.getWidth(), enemyImage.getHeight(), matrix3, true);
-        enemy4 = Bitmap.createBitmap(enemyImage, 0, 0, enemyImage.getWidth(), enemyImage.getHeight(), matrix4, true);
+        enemyCenterX = p.x + (enemyImage[0].getWidth()) / 2;
+        enemyCenterY = p.y + (enemyImage[0].getHeight()) / 2;
 
         count = 0;
     }
 
     public void move(){
         p.y += moveSpeed * scale;
-        enemyCenterX = p.x + (sizeX) / 2;
-        enemyCenterY = p.y + (sizeY) / 2;
+        enemyCenterX = p.x + (enemyImage[0].getWidth()) / 2;
+        enemyCenterY = p.y + (enemyImage[0].getHeight()) / 2;
     }
 
     public void drawMove(Canvas c){
@@ -76,13 +54,13 @@ class Enemy{
         count++;
 
         if(count < 5){
-            c.drawBitmap(enemy1, p.x, p.y, paint);
+            c.drawBitmap(enemyImage[0], p.x, p.y, paint);
         }else if(5 <= count && count < 10){
-            c.drawBitmap(enemy2, p.x, p.y, paint);
+            c.drawBitmap(enemyImage[1], p.x, p.y, paint);
         }else if(10 <= count && count < 15){
-            c.drawBitmap(enemy3, p.x, p.y, paint);
+            c.drawBitmap(enemyImage[2], p.x, p.y, paint);
         }else{
-            c.drawBitmap(enemy4, p.x, p.y, paint);
+            c.drawBitmap(enemyImage[3], p.x, p.y, paint);
         }
 
         if(count >= 20){
